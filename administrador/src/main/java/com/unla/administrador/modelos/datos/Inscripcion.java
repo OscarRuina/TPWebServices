8dd1126735fae646c57ed51176842618b8cfcf8c
Oscar <oscarruina@hotmail.com>
2022-10-12 08:24:59 -0300

    Agrego modelo de inscripciones, punto e

---
+++ administrador/src/main/java/com/unla/administrador/modelos/datos/Inscripcion.java
@@ -0,0 +1,35 @@
+package com.unla.administrador.modelos.datos;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "INSCRIPCIONES")
+public class Inscripcion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private long id;
+
+    @Column(name = "inicio")
+    private LocalDate inicio;
+
+    @Column(name = "fin")
+    private LocalDate fin;
+
+}
---
+++ administrador/src/main/java/com/unla/administrador/modelos/dtos/respuesta/RespuestaInscripcion.java
@@ -0,0 +1,19 @@
+package com.unla.administrador.modelos.dtos.respuesta;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RespuestaInscripcion {
+
+    private long id;
+
+    private LocalDate inicio;
+
+    private LocalDate fin;
+
+}

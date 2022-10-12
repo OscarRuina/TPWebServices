d79214136dcc460cc86ebefb19f56ba945e62023
Oscar <oscarruina@hotmail.com>
2022-10-12 08:29:48 -0300

    Agrego repositorio de inscripciones, punto e

---
+++ administrador/src/main/java/com/unla/administrador/convertidores/InscripcionConvertidor.java
@@ -0,0 +1,18 @@
+package com.unla.administrador.convertidores;
+
+import com.unla.administrador.modelos.datos.Inscripcion;
+import com.unla.administrador.modelos.dtos.respuesta.RespuestaInscripcion;
+
+public final class InscripcionConvertidor {
+
+    private InscripcionConvertidor(){}
+
+    public static RespuestaInscripcion convertirRespuestaInscripcion(Inscripcion inscripcion){
+        RespuestaInscripcion dto = new RespuestaInscripcion();
+        dto.setId(inscripcion.getId());
+        dto.setInicio(inscripcion.getInicio());
+        dto.setFin(inscripcion.getFin());
+        return dto;
+    }
+
+}

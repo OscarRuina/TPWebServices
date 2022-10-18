20dcb38f57ecaec2b29b39171075eb91a4876911
Oscar Ruina <31217980+OscarRuina@users.noreply.github.com>
2022-10-18 19:39:13 -0300

    Modulo Estudiante: Punto C (#19)
    * Modifico nombres de clases
    
    * Agrego servicio soap para el punto a y b
    
    * Agrego enpoint para inscribirse y darse de baja en materias.
    
    * Agrego enpoints para agregar y dar de baja mesa de examen
    
    * Agrego ednpoints y servicios de listado de materias y mesas de examen
    
    * Agrego servicio para el punto c

--- estudiante/src/main/java/com/unla/estudiante/repositorios/NotaFinalRepositorio.java
+++ estudiante/src/main/java/com/unla/estudiante/repositorios/NotaFinalRepositorio.java
@@ -1,6 +1,7 @@
 package com.unla.estudiante.repositorios;
 
 import com.unla.estudiante.modelos.datos.NotaFinal;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface NotaFinalRepositorio extends JpaRepository<NotaFinal,Long> {
 
     NotaFinal findByEstudiante_IdAndMesaExamen_Id(long idUsuario, long idMesaExamen);
+
+    List<NotaFinal> findByEstudiante_IdAndAprobadoTrue(long id);
 
 }
